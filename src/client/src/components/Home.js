bea9a6217009da263a91fa4fc3bec20c65e9eb17
Morgan Mason/Work <morganmason0606@gmail.com>
2022-11-11 22:06:54 -0800

    Changed the home page to new version: from test display of rides to a summary of our purpose and mission ride summary kept at bottom as example of rides
    link have not been connected yet; hopefully can have them link to the corresponding pages on our website
    I also plan to add an 'about us' section at the bottom for us to input information

--- src/client/src/components/Home.js
+++ src/client/src/components/Home.js
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import logo from '../blue.png';
 
+import kodi from "./photo/kodi.png"
+
 import '../App.css';
 import RideShareComponent from './RideShareComponent'
 
@@ -32,6 +34,51 @@ function Home() {
   }
 
   return (
+<div className="App">
+<div className="container"> 
+  <h2> <img src={logo} alt="Logo" width="90" /> Bruin Rides </h2>
+</div>
+<div className="homepageText">
+  <p>
+  <b>Bruin Rides</b> is a website where students from UCLA can coordinate sharing an Uber or Lyft ride.
+  </p>
+  <br></br>
+  <p>
+  Whether it's sightseers looking for fun in <a href="google.com">downtown LA</a>, beach-goers headed to <a href ="google.com">Santa Monica</a>,
+  or even Bruins hoping to get a better rate on their trip to <a href="google.com">LAX</a>, 
+  Bruin Rides hopes to be the central location for students to coordinate splitting fares.
+  </p>
+  <br></br>
+  <hr></hr>
+  <br></br>
+  <p>
+  <b>To get started</b> <a href="google.com">log in or sign up</a> and search for your ride by: destination, date, and time. 
+  </p><p>If you can't find a ride you want, take the initiative and <a href="google.com">make your own ride</a> for others to join in on. 
+  </p>
+  <br></br>
+  <br></br>
+  <br></br>
+  <h3>Some Rides On Our Page Now</h3>
+</div>
+<div className="list" font-size="20px">
+          {rides.map(ride => <RideShareComponent
+            key={ride._id}
+            remove={() => deleteRide(ride._id)}
+            region={ride.region}
+            destination={ride.destination}
+            date={ride.date}
+            time={ride.time}
+            desc={ride.desc}
+          />)}
+</div>
+
+</div>
+  );
+}
+
+export default Home;
+
+  /*
     <div className="App">
       <div className="container"> 
         <h2> <img src={logo} alt="Logo" width="90" /> Bruin Rides </h2>
@@ -43,7 +90,7 @@ function Home() {
             onChange={(e) => setText(e.target.value)} />
           <div className="add"
             onClick={addRide}>{"Add"}</div>
-        </div> */}
+        </div> }
 
         <div className="list">
           {rides.map(ride => <RideShareComponent
@@ -59,7 +106,32 @@ function Home() {
 
       </div>
     </div>
-  );
-}
+*/
 
-export default Home;
+/*
+<div className="homepageAboutUs">
+<figure>
+  <img src="https://via.placeholder.com/150" alt="Trulli" style="width:40%"/>
+  <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+</figure>
+<figure>
+  <img src="https://via.placeholder.com/150" alt="Trulli" style="width:40%"/>
+  <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+</figure>
+
+<br></br>
+<figure>
+  <img src="https://via.placeholder.com/150" alt="Trulli" style="width:30%"/>
+  <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+</figure>
+<figure>
+  <img src="https://via.placeholder.com/150" alt="Trulli" style="width:30%"/>
+  <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+</figure>
+<figure>
+  <img src="https://via.placeholder.com/150" alt="Trulli" style="width:30%"/>
+  <figcaption>Fig.1 - Trulli, Puglia, Italy.</figcaption>
+</figure>
+
+</div>
+*/
