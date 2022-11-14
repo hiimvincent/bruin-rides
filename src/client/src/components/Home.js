5c304f7a75ea9a2f9333cb74a116c5ea31d57961
morganmason0606 <46251497+morganmason0606@users.noreply.github.com>
2022-11-13 19:15:13 -0800

    Update Home.js

--- src/client/src/components/Home.js
+++ src/client/src/components/Home.js
@@ -2,7 +2,6 @@ import { useState, useEffect } from 'react';
 import axios from "axios";
 import logo from '../blue.png';
 
-import kodi from "./photo/kodi.png"
 
 import '../App.css';
 import RideShareComponent from './RideShareComponent'
