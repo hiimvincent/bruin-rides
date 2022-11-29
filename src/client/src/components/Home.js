b0c0bfc4797b28805011aae37211bab2822f8451
David Lee <davidlee@vpn-172-27-17-138.host.ucla.edu>
2022-11-29 13:49:00 -0800

    Cleaning style and alignment of About page

--- src/client/src/components/Home.js
+++ src/client/src/components/Home.js
@@ -17,28 +17,30 @@ function Home() {
 <center>
   <div className="row">
     <div className="homepageText largeCol">
-      <p>
-      <b>Bruin Rides</b> offers UCLA students a platform to coordinate carpooling.
-      </p>
       <br></br>
-      <p>
-      Whether it's sightseers looking for fun in downtown LA, beach-goers headed to Santa Monica,
-      or even Bruins hoping to get a better rate on their trip to LAX, 
-      Bruin Rides is the central location for all UCLA students looking to split transportation costs.
-      </p>
-      <br></br>
-      <p>Find rides created by fellow Bruins or create your own ride for others to join. </p> 
-      <br></br>
-      <hr></hr>
-      <br></br>
-      <p>
-      <b>To get started</b> log in or sign up and search for rides by destination, date, or time. 
-      </p>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-
+      <div className="aform">
+        <form>
+          <br></br>
+          <p>
+          <b>Bruin Rides</b> offers UCLA students a platform to coordinate carpooling.
+          </p>
+          <br></br>
+          <p>
+          Whether it's sightseers looking for fun in downtown LA, beach-goers headed to Santa Monica,
+          or even Bruins hoping to get a better rate on their trip to LAX, 
+          Bruin Rides is the central location for all UCLA students looking to split transportation costs.
+          </p>
+          <br></br>
+          <p>Find rides created by fellow Bruins or create your own ride for others to join. </p> 
+          <br></br>
+          <hr></hr>
+          <br></br>
+          <p>
+          <b>To get started</b> log in or sign up and search for rides by destination, date, or time. 
+          </p>
+          <br></br>
+        </form>
+      </div>
       {/* <h3>Some Rides On Our Page Now</h3>
     </div>
     <div className="list" font-size="20px">
@@ -51,8 +53,8 @@ function Home() {
                 time={ride.time}
                 desc={ride.desc}
               />)} */}
-    </div>
-    <div className="smallCol">
+  </div>
+    <div className="imgCol">
       <img className="homeImage" src ={bear}/>
     </div>
   </div>
